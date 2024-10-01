1563a541d26ea1ffb8e5d34a04f2bc49bcc50d58
Jimena Luna <jimena1@ad.unc.edu>
2024-10-01 17:56:17 -0400

    Your commit message

---
+++ f24-bootcamp-unit-converter/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  f24-bootcamp-unit-converter
+//
+//  Created by jimena rivera on 10/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            Text("Check git")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
