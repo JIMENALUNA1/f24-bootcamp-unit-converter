aed446a30e05325280b6080022a21892eab05632
Jimena Luna <156983146+JIMENALUNA1@users.noreply.github.com>
2024-12-04 19:08:54 -0500

    Update ContentView.swift

--- f24-bootcamp-unit-converter/ContentView.swift
+++ f24-bootcamp-unit-converter/ContentView.swift
@@ -3,23 +3,82 @@
 //  f24-bootcamp-unit-converter
 //
 //  Created by jimena rivera on 10/1/24.
-//
 
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-            Text("Check git")
-        }
-        .padding()
-    }
+    @State var inputValue = 0.0
+  @State var inputUnit = "mL"
+  @State var convertTo = "mL"
+
+  let units = ["mL", "Liters", "Cups", "Pints", "Gallons"]
+  
+  let literToMilliliter = 1000.0
+  let cupToMilliliter = 236.588
+  let pintToMilliliter = 473.176
+  let gallonToMilliliter = 3785.41
+
+  var inputUnitToMilliliter: Double {
+      switch inputUnit {
+      case "mL":
+          return inputValue
+      case "Liters":
+          return inputValue * literToMilliliter
+      case "Cups":
+          return inputValue * cupToMilliliter
+      case "Pints":
+          return inputValue * pintToMilliliter
+      default:
+          return inputValue * gallonToMilliliter
+      }
+  }
+
+  var outputValue: Double {
+      switch convertTo {
+      case "mL":
+          return inputUnitToMilliliter
+      case "Liters":
+          return inputUnitToMilliliter / literToMilliliter
+      case "Cups":
+          return inputUnitToMilliliter / cupToMilliliter
+      case "Pints":
+          return inputUnitToMilliliter / pintToMilliliter
+      default:
+          return inputUnitToMilliliter / gallonToMilliliter
+      }
+  }
+
+  var body: some View {
+      NavigationStack {
+          Form {
+              Section("Input Value and Unit") {
+                  TextField("Input Value", value: $inputValue, format: .number)
+                  Picker("Unit", selection: $inputUnit) {
+                      ForEach(units, id: \.self) { unit in
+                          Text(unit)
+                      }
+                  }
+                  .pickerStyle(.segmented)
+              }
+              Section("Convert To") {
+                  Picker("Unit", selection: $convertTo) {
+                      ForEach(units, id: \.self) { unit in
+                          Text(unit)
+                      }
+                  }
+                  .pickerStyle(.segmented)
+              }
+              Section("Output") {
+                  Text("\(outputValue, specifier: "%.2f")")
+              }
+          }
+          .navigationTitle("Volume Converter")
+      }
+  }
+}
+#Preview {
+  ContentView()
 }
 
-#Preview {
-    ContentView()
-}
+
+
